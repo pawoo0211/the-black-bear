b3663373ce29eeb2062d7c0498344f0b2f9b333f
pawoo0211 <pawoo0211@naver.com>
2024-10-13 23:27:37 +0900

    initial commit

---
+++ src/main/java/com/blackbear/project/ThrBlackBearApplication.java
@@ -0,0 +1,12 @@
+package com.blackbear.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThrBlackBearApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ThrBlackBearApplication.class, args);
+	}
+
+}
